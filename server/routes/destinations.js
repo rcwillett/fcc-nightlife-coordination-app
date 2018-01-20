bd9bcbea0978893b5b1cae47157d0629747f3324
Ross <oblivion1214@hotmail.com>
2018-01-20 23:46:02 +0000

    Added caching for external api request

--- server/routes/destinations.js
+++ server/routes/destinations.js
@@ -1,25 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+const NodeCache  = require('node-cache');
+let destCache = new NodeCache({ stdTTL: 21600, checkperiod: 0 });
 
-router.get('/search-destinations', function(req, res, next){
+router.get('/search-destinations', handleSearchRequest);
+
+module.exports = router;
+
+function handleSearchRequest (req, res, next) {
+    let cacheKey = req.query.search.replace(/^[\w]/, '').toLowerCase();
+    destCache.get(cacheKey, function(err, value){
+        if (err || value === undefined){
+            getYelpSearchResults(req, res, next, cacheKey)
+        }
+        else{
+            res.json(value);
+        }
+    });
+}
+
+function getYelpSearchResults (req, res, next, cacheKey) {
     let requestInfo = {
         headers: {
             'Authorization': "Bearer " + process.env.YELP_API_KEY
         },
         qs: {
-            'location': req.query.search
+            'location': req.query.search,
+            'categories': 'Nightlife'
         },
         url: "https://api.yelp.com/v3/businesses/search"
     };
     request.get(requestInfo, function(err, resp, body){
-        if(err || resp.statusCode !== 200){
+        let parsedResponse;
+        if (err || resp.statusCode !== 200){
             next(new Error(err));
         }
-        else{
-            res.json(JSON.parse(body));
+        else {
+            parsedResponse = JSON.parse(body);
+            destCache.set(cacheKey, parsedResponse);
+            res.json(parsedResponse);
         }
     });
-});
-
-module.exports = router;
+}
