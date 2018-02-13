0620c7c820b46cb6660c908adee7cff02be293e2
Ross <oblivion1214@hotmail.com>
2018-02-13 23:47:28 +0000

    Started adding methods to mongoose object

--- server/models/destinations.js
+++ server/models/destinations.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
-const destinationSchema = new mongoose.Schema({
+let destinationSchema = new mongoose.Schema({
         id: String,
         //Note, use email to track users going
-        usersGoing: [String]
+        attendants: [String]
 });
-    
+
+destinationSchema.method.addAttendant = function(destId, userId, callback, next) {
+        this.model("destination").update(destId, { "$addToSet": { "attendants": userId } }, { "new": true, "upsert": true },
+                function(err, result) {
+                        if (err) return next(err);
+                        callback(null, true);
+                });
+};
+
 module.exports = mongoose.model("destination", destinationSchema);
