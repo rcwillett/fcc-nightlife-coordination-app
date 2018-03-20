f3844bc33b3098336e63a5516f91508c96416184
Ross <oblivion1214@hotmail.com>
2018-03-20 21:14:49 +0000

    Added styles to app compilation

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/app.jsx';
+import AppStyles from './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('ReactApp'));
